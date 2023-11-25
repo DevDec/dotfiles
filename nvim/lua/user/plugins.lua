8ce5630a2833c4498eda9abfb700589f3fea154c
Declan Brown <enquire@declanbrown.co.uk>
2023-11-25 16:05:53 +0000

    Packer > Lazy package manager migration
    I've migrated over to the Lazy package manager. The only reason for this is Packer.nvim is no longer maintained
    The author of Packer recommended moving to lazy.nvim as this is now the most stable package manager.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,333 +1,295 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-require('packer').reset()
-require('packer').init({
-  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'solid' })
-    end,
-  },
-})
-
-local use = require('packer').use
-
--- Packer can manage itself.
-use('wbthomason/packer.nvim')
-
 -- One Dark theme.
-use({
-  'jessarcher/onedark.nvim',
-  config = function()
-    vim.cmd('colorscheme onedark')
+require("lazy").setup({
+    {
+      'jessarcher/onedark.nvim',
+      config = function()
+        vim.cmd('colorscheme onedark')
 
-    vim.api.nvim_set_hl(0, 'FloatBorder', {
-      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-    })
+        vim.api.nvim_set_hl(0, 'FloatBorder', {
+            fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+          })
 
-    -- Make the cursor line background invisible
-    vim.api.nvim_set_hl(0, 'CursorLineBg', {
-      fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-      bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-    })
+        -- Make the cursor line background invisible
+        vim.api.nvim_set_hl(0, 'CursorLineBg', {
+            fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+          })
 
-    vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+        vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
 
-    vim.api.nvim_set_hl(0, 'StatusLineNonText', {
-      fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
-      bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
-    })
+        vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+            fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+            bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+          })
 
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
-  end,
-})
+        vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+      end,
+    },
 
--- Commenting support.
-use('tpope/vim-commentary')
+    -- Commenting support.
+    {'tpope/vim-commentary'},
 
--- Add, change, and delete surrounding text.
-use('tpope/vim-surround')
+    -- Add, change, and delete surrounding text.
+    {'tpope/vim-surround'},
 
--- Useful commands like :Rename and :SudoWrite.
-use('tpope/vim-eunuch')
+    -- Useful commands like :Rename and :SudoWrite.
+    {'tpope/vim-eunuch'},
 
--- Pairs of handy bracket mappings, like [b and ]b.
-use('tpope/vim-unimpaired')
+    -- Pairs of handy bracket mappings, like [b and ]b.
+    {'tpope/vim-unimpaired'},
 
--- Indent autodetection with editorconfig support.
-use('tpope/vim-sleuth')
+    -- Indent autodetection with editorconfig support.
+    {'tpope/vim-sleuth'},
 
--- Allow plugins to enable repeating of commands.
-use('tpope/vim-repeat')
+    -- Allow plugins to enable repeating of commands.
+    {'tpope/vim-repeat'},
 
--- Add more languages.
-use('sheerun/vim-polyglot')
+    -- Add more languages.
+    {'sheerun/vim-polyglot'},
 
--- Navigate seamlessly between Vim windows and Tmux panes.
-use('christoomey/vim-tmux-navigator')
+    -- Navigate seamlessly between Vim windows and Tmux panes.
+    {'christoomey/vim-tmux-navigator'},
 
--- Jump to the last location when opening a file.
-use('farmergreg/vim-lastplace')
+    -- Jump to the last location when opening a file.
+    {'farmergreg/vim-lastplace'},
 
--- Enable * searching with visually selected text.
-use('nelstrom/vim-visual-star-search')
+    -- Enable * searching with visually selected text.
+    {'nelstrom/vim-visual-star-search'},
 
--- Automatically create parent dirs when saving.
-use('jessarcher/vim-heritage')
+    -- Automatically create parent dirs when saving.
+    {'jessarcher/vim-heritage'},
 
--- Text objects for HTML attributes.
-use({
-  'whatyouhide/vim-textobj-xmlattr',
-  requires = 'kana/vim-textobj-user',
-})
+    -- Text objects for HTML attributes.
+    {
+      'whatyouhide/vim-textobj-xmlattr',
+      dependencies = 'kana/vim-textobj-user',
+    },
 
--- Automatically set the working directory to the project root.
-use({
-  'airblade/vim-rooter',
-  setup = function()
-    -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
-    vim.g.rooter_manual_only = 1
-  end,
-  config = function()
-    vim.cmd('Rooter')
-  end,
-})
+    -- Automatically set the working directory to the project root.
+    {
+      'airblade/vim-rooter',
+      init = function()
+        -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+        vim.g.rooter_manual_only = 1
+      end,
+      config = function()
+        vim.cmd('Rooter')
+      end,
+    },
 
--- Automatically add closing brackets, quotes, etc.
-use({
-  'windwp/nvim-autopairs',
-  config = function()
-    require('nvim-autopairs').setup()
-  end,
-})
+    -- Automatically add closing brackets, quotes, etc.
+    {
+      'windwp/nvim-autopairs',
+      config = function()
+        require('nvim-autopairs').setup()
+      end,
+    },
 
--- Add smooth scrolling to avoid jarring jumps
-use({
-  'karb94/neoscroll.nvim',
-  config = function()
-    require('neoscroll').setup()
-  end,
-})
+    -- Add smooth scrolling to avoid jarring jumps
+    {
+      'karb94/neoscroll.nvim',
+      config = function()
+        require('neoscroll').setup()
+      end,
+    },
 
--- All closing buffers without closing the split window.
-use({
-  'famiu/bufdelete.nvim',
-  config = function()
-    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
-  end,
-})
+    -- All closing buffers without closing the split window.
+    {
+      'famiu/bufdelete.nvim',
+      config = function()
+        vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+      end,
+    },
 
--- Split arrays and methods onto multiple lines, or join them back up.
-use({
-  'AndrewRadev/splitjoin.vim',
-  config = function()
-    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-    vim.g.splitjoin_trailing_comma = 1
-    vim.g.splitjoin_php_method_chain_full = 1
-  end,
-})
+    -- Split arrays and methods onto multiple lines, or join them back up.
+    {
+      'AndrewRadev/splitjoin.vim',
+      config = function()
+        vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+        vim.g.splitjoin_trailing_comma = 1
+        vim.g.splitjoin_php_method_chain_full = 1
+      end,
+    },
 
--- Automatically fix indentation when pasting code.
-use({
-  'sickill/vim-pasta',
-  config = function()
-    vim.g.pasta_disabled_filetypes = { 'fugitive' }
-  end,
-})
+    -- Automatically fix indentation when pasting code.
+    {
+      'sickill/vim-pasta',
+      config = function()
+        vim.g.pasta_disabled_filetypes = { 'fugitive' }
+      end,
+    },
 
--- Fuzzy finder
-use({
-  'nvim-telescope/telescope.nvim',
-  requires = {
-    'nvim-lua/plenary.nvim',
-    'kyazdani42/nvim-web-devicons',
-    'nvim-telescope/telescope-live-grep-args.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-  },
-  config = function()
-    require('user/plugins/telescope')
-  end,
-})
+    -- Fuzzy finder
+    {
+      'nvim-telescope/telescope.nvim',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons',
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      },
+      config = function()
+        require('user/plugins/telescope')
+      end,
+    },
 
--- File tree sidebar
-use({
-  'kyazdani42/nvim-tree.lua',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config = function()
-    require('user/plugins/nvim-tree')
-  end,
-})
+    -- File tree sidebar
+    {
+      'kyazdani42/nvim-tree.lua',
+      dependencies = 'kyazdani42/nvim-web-devicons',
+      config = function()
+        require('user/plugins/nvim-tree')
+      end,
+    },
 
--- A Status line.
-use({
-  'nvim-lualine/lualine.nvim',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config = function()
-    require('user/plugins/lualine')
-  end,
-})
+    -- A Status line.
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = 'kyazdani42/nvim-web-devicons',
+      config = function()
+        require('user/plugins/lualine')
+      end,
+    },
 
--- Add a dashboard.
-use({
-  'glepnir/dashboard-nvim',
-  config = function()
-    require('user/plugins/dashboard-nvim')
-  end
-})
+    -- Add a dashboard.
+    {
+      'glepnir/dashboard-nvim',
+      config = function()
+        require('user/plugins/dashboard-nvim')
+      end
+    },
 
--- Git integration.
-use({
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup({current_line_blame = true})
-    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
-    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
-    vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
-    vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
-    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
-    vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
-  end,
-})
+    -- Git integration.
+    {
+      'lewis6991/gitsigns.nvim',
+      config = function()
+        require('gitsigns').setup({current_line_blame = true})
+        vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+        vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+        vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+        vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+        vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+        vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+      end,
+    },
 
--- Git commands.
-use({
-  'tpope/vim-fugitive',
-  requires = 'tpope/vim-rhubarb',
-})
+    -- Git commands.
+    {
+      'tpope/vim-fugitive',
+      dependencies = 'tpope/vim-rhubarb',
+    },
 
---- Floating terminal.
-use({
-  'voldikss/vim-floaterm',
-  config = function()
-    vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.8
-    vim.keymap.set('n', '<Tab>t', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<Tab>t', '<C-\\><C-n>:FloatermToggle<CR>')
-    vim.cmd([[
-      highlight link Floaterm CursorLine
+    --- Floating terminal.
+    {
+      'voldikss/vim-floaterm',
+      config = function()
+        vim.g.floaterm_width = 0.8
+        vim.g.floaterm_height = 0.8
+        vim.keymap.set('n', '<Tab>t', ':FloatermToggle<CR>')
+        vim.keymap.set('t', '<Tab>t', '<C-\\><C-n>:FloatermToggle<CR>')
+        vim.cmd([[
+          highlight link Floaterm CursorLine
       highlight link FloatermBorder CursorLineBg
     ]])
-  end
-})
-
--- Improved syntax highlighting
-use({
-  'nvim-treesitter/nvim-treesitter',
-  run = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
-  requires = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  config = function()
-    require('user/plugins/treesitter')
-  end,
-})
-
--- Language Server Protocol.
-use({
-  'neovim/nvim-lspconfig',
-  requires = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'b0o/schemastore.nvim',
-    'jose-elias-alvarez/null-ls.nvim',
-    'jayp0521/mason-null-ls.nvim',
-  },
-  config = function()
-    require('user/plugins/lspconfig')
-  end,
-})
-
--- Completion
-use({
-  'hrsh7th/nvim-cmp',
-  requires = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'onsails/lspkind-nvim',
-  },
-  config = function()
-    require('user/plugins/cmp')
-  end,
-})
-
--- PHP Refactoring Tools
-use({
-  'phpactor/phpactor',
-  ft = 'php',
-  run = 'composer install --no-dev --optimize-autoloader',
-  config = function()
-    vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
-    vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
-  end,
-})
-
--- Project Configuration.
-use({
-  'tpope/vim-projectionist',
-  requires = 'tpope/vim-dispatch',
-  config = function()
-    require('user/plugins/projectionist')
-  end,
-})
-
--- Testing helper
-use({
-  'vim-test/vim-test',
-  config = function()
-    require('user/plugins/vim-test')
-  end,
-})
-
-use ({
-    'ThePrimeagen/harpoon',
-    config = function()
-      require('harpoon').setup({
-          tabline = true
-        })
-      vim.keymap.set('n', '<Leader>ho', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
-      vim.keymap.set('n', '<Leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
-    end,
-  })
-
-use({
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    requires = {
-        "nvim-lua/plenary.nvim",
+      end
     },
-    config = function()
-      vim.keymap.set('n', '<Leader>l', ':LazyGit<CR>')
-    end,
-})
 
--- Automatically set up your configuration after cloning packer.nvim
--- Put this at the end after all plugins
-if packer_bootstrap then
-    require('packer').sync()
-end
+    -- Improved syntax highlighting
+    {
+      'nvim-treesitter/nvim-treesitter',
+      build = function()
+        require('nvim-treesitter.install').update({ with_sync = true })
+      end,
+      dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+      config = function()
+        require('user/plugins/treesitter')
+      end,
+    },
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile>
-  augroup end
-]])
+    -- Language Server Protocol.
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'b0o/schemastore.nvim',
+        'jose-elias-alvarez/null-ls.nvim',
+        'jayp0521/mason-null-ls.nvim',
+      },
+      config = function()
+        require('user/plugins/lspconfig')
+      end,
+    },
+
+    -- Completion
+    {
+      'hrsh7th/nvim-cmp',
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'onsails/lspkind-nvim',
+      },
+      config = function()
+        require('user/plugins/cmp')
+      end,
+    },
+
+    -- PHP Refactoring Tools
+    {
+      'phpactor/phpactor',
+      ft = 'php',
+      build = 'composer install --no-dev --optimize-autoloader',
+      config = function()
+        vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
+        vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
+      end,
+    },
+
+    -- Project Configuration.
+    {
+      'tpope/vim-projectionist',
+      dependencies = 'tpope/vim-dispatch',
+      config = function()
+        require('user/plugins/projectionist')
+      end,
+    },
+
+    -- Testing helper
+    {
+      'vim-test/vim-test',
+      config = function()
+        require('user/plugins/vim-test')
+      end,
+    },
+
+    {
+      'ThePrimeagen/harpoon',
+      config = function()
+        require('harpoon').setup({
+            tabline = true
+          })
+        vim.keymap.set('n', '<Leader>ho', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+        vim.keymap.set('n', '<Leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
+      end,
+    },
+
+    {
+      "kdheepak/lazygit.nvim",
+      -- optional for floating window border decoration
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      config = function()
+        vim.keymap.set('n', '<Leader>l', ':LazyGit<CR>')
+      end,
+    }
+
+  })
