b81ff11fbd78a969a4956b3a6a8e5d404779b440
Declan Brown <enquire@declanbrown.co.uk>
2023-11-25 17:47:44 +0000

    Adds yamlls language server

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -6,6 +6,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 
 -- PHP
 require('lspconfig').intelephense.setup({ capabilities = capabilities })
+require('lspconfig').yamlls.setup({ capabilities = capabilities })
 
 -- Vue, JavaScript, TypeScript
 require('lspconfig').volar.setup({
