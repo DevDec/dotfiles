2eec8004353ebc8733e742640894baaca8c12450
DevDec <33901712+DevDec@users.noreply.github.com>
2024-02-11 15:54:15 +0000

    Updates harpoon to version 2

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -253,17 +253,44 @@ require("lazy").setup({
         require('user/plugins/vim-test')
       end,
     },
-
     {
-      'ThePrimeagen/harpoon',
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" },
       config = function()
-        require('harpoon').setup({
-            tabline = true
-          })
-        vim.keymap.set('n', '<Leader>ho', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
-        vim.keymap.set('n', '<Leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
-      end,
+        local harpoon = require('harpoon')
+        harpoon:setup({})
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+          local file_paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(file_paths, item.value)
+          end
+
+          require("telescope.pickers").new({}, {
+              prompt_title = "Harpoon",
+              finder = require("telescope.finders").new_table({
+                  results = file_paths,
+                }),
+              previewer = conf.file_previewer({}),
+              sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<Leader>ho", function() toggle_telescope(harpoon:list()) end,
+          { desc = "Open harpoon window" })
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+      end
     },
+    -- {
+    --   'ThePrimeagen/harpoon',
+    --   config = function()
+    --     require('harpoon').setup()
+    --     vim.keymap.set('n', '<Leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
+    --     vim.keymap.set('n', '<Leader>ho', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+    --   end,
+    -- },
 
     {
       "kdheepak/lazygit.nvim",
