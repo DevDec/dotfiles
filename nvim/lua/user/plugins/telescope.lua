3b739e7b026d6bcc9993b4ff81b98e6506b57ee6
enquire@declanbrown.co.uk <enquire@declanbrown.co.uk>
2024-03-21 17:53:14 +0000

    Moves customer telescope extensions into their own file

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,60 +1,5 @@
-local actions = require('telescope.actions')
 local lga_actions = require("telescope-live-grep-args.actions")
-local action_state = require "telescope.actions.state";
-local utils = require "telescope.utils"
-
---- Ask user to confirm an action
----@param prompt string: The prompt for confirmation
----@param default_value string: The default value of user input
----@param yes_values table: List of positive user confirmations ({"y", "yes"} by default)
----@return boolean: Whether user confirmed the prompt
-local function ask_to_confirm(prompt, default_value, yes_values)
-  yes_values = yes_values or { "y", "yes" }
-  default_value = default_value or ""
-  local confirmation = vim.fn.input(prompt, default_value)
-  confirmation = string.lower(confirmation)
-  if string.len(confirmation) == 0 then
-    return false
-  end
-  for _, v in pairs(yes_values) do
-    if v == confirmation then
-      return true
-    end
-  end
-  return false
-end
-
-local git_drop_stash = function(prompt_bufnr)
-  local selection = action_state.get_selected_entry()
-
-  local confirmation = ask_to_confirm(string.format("Drop stash '%s'? [y/n]: ", selection.value))
-  if not confirmation then
-    utils.notify("actions.git_drop_stash", {
-        msg = string.format("Drop stash canceled: '%s'", selection.value),
-        level = "INFO",
-      })
-    return
-  end
-
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  picker:delete_selection(function(selection)
-    local _, ret, stderr = utils.get_os_command_output { "git", "stash", "drop", selection.value }
-
-    if ret == 0 then
-      utils.notify("actions.git_drop_stash", {
-          msg = string.format("Dropped stash: '%s' ", selection.value),
-          level = "INFO",
-        })
-    else
-      utils.notify("actions.git_apply_stash", {
-          msg = string.format("Error when dropping: %s. Git returned: '%s'", selection.value, table.concat(stderr, " ")),
-          level = "ERROR",
-        })
-    end    
-
-    return ret == 0
-  end)
-end
+local telescope_custom_extensions = require('telescope-custom-extensions')
 
 require('telescope').setup({
     extensions= {
@@ -105,7 +50,7 @@ require('telescope').setup({
       git_stash = {
         mappings = {
           n = {
-            ["<c-d>"] = git_drop_stash,
+            ["<c-d>"] = telescope_custom_extensions.git.drop_stash,
           },
         },
       }
